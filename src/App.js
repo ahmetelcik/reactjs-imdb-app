f413435975ce8d142d7eeec93422e9f041c4ea29
Bünyamin Akın <ceng.akin@gmail.com>
2017-08-05 17:59:01 +0300

    controlled API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import superagent from 'superagent';
 import './App.css';
 
 class App extends Component {
+
+  getFilm = () => {
+    const url = "https://api.themoviedb.org/3/search/movie?";
+    const params = {
+      api_key:"f681d442d64e8d1a866ef54693c4e2b0",
+      query:"arrival"
+    };
+
+    superagent
+      .get(url)
+      .query(params)
+      .set("Accept", "application/json")
+      .then((data) => {
+        console.log(data);
+        console.log(url);
+      },(err) => {
+        console.log(err.message);
+      });
+  };
+
   render() {
     return (
       <div className="App">
+        <a onClick={this.getFilm} >Click Me</a>
       </div>
     );
   }
